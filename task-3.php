8c4423e78a74d39cbc2bdf2ac9954381ccca3b10
shutkos <shutkos@gmail.com>
2021-12-28 20:03:24 +0200

    init

---
+++ task-3.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task-3</title>
+    <style>
+        ul{
+            display: block;
+        }
+        li{
+            display: inline-block;
+        }
+        a{
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+<?php
+$data = [
+    [
+        'title' => 'Home',
+        'link' => '/'
+    ],
+    [
+        'title' => 'PHP',
+        'link' => '/php'
+    ],
+    [
+        'title' => 'functions',
+        'link' => '/php/functions'
+    ]
+]; ?>
+<ul>
+    <?php
+    foreach ($data as $val) {
+        if ($val === end($data)) {
+            ?>
+            <li><?=$val['title']?></li>
+            <?php
+        } else {
+            ?>
+            <li><a href="<?=$val['link']?>"><?=$val['title']?> > </a></li>
+            <?php
+        }
+    }
+    ?>
+
+</ul>
+
+
+</body>
+</html>
